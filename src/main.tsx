395224a6029d236a16a1bf7fa37ee56658bcb90a
Ivan Li <ivanli2048@gmail.com>
2025-11-29 23:36:17 +0800

    feat: add daisyui theme and system sync
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,38 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+const LIGHT_THEME = "winter";
+const DARK_THEME = "dim";
+
+function setupSystemThemeSync() {
+  if (typeof window === "undefined") {
+    return;
+  }
+
+  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const applyTheme = (isDark: boolean) => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDark ? DARK_THEME : LIGHT_THEME,
+    );
+  };
+
+  applyTheme(mediaQuery.matches);
+
+  const listener = (event: MediaQueryListEvent) => {
+    applyTheme(event.matches);
+  };
+
+  if (typeof mediaQuery.addEventListener === "function") {
+    mediaQuery.addEventListener("change", listener);
+  } else if (typeof mediaQuery.addListener === "function") {
+    mediaQuery.addListener(listener);
+  }
+}
+
+setupSystemThemeSync();
+
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
